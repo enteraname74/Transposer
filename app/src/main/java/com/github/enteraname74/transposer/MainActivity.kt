3ae1d8cf992a0fbd4191d747de21e6ae64a0fee9
enteraname74 <noahpenin@gmail.com>
2022-09-28 09:47:03 +0200

    Add basic file for the project

---
+++ app/src/main/java/com/github/enteraname74/transposer/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.github.enteraname74.transposer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.github.enteraname74.Transposer.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
