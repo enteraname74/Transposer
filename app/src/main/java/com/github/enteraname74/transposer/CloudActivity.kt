6f0e34a708ba2a41a76536db50d4c0b0c9714957
enteraname74 <noahpenin@gmail.com>
2022-09-30 19:31:55 +0200

    Add button for going to the cloud activity

---
+++ app/src/main/java/com/github/enteraname74/transposer/CloudActivity.kt
@@ -0,0 +1,11 @@
+package com.github.enteraname74.transposer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CloudActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cloud)
+    }
+}
