5d4c6d9cdf5e662cf96a1ef6d04caf4a872dd8a8
enteraname74 <noahpenin@gmail.com>
2022-09-30 19:23:05 +0200

    Add basic style for the app

--- app/src/main/java/com/github/enteraname74/transposer/MainActivity.kt
+++ app/src/main/java/com/github/enteraname74/transposer/MainActivity.kt
@@ -2,7 +2,6 @@ package com.github.enteraname74.transposer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.github.enteraname74.Transposer.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
