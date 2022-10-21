24608bb39d89d456cedbb1ad0df15f2a3b820c80
Tommy DI LUNA <40468597+Lotopi@users.noreply.github.com>
2022-10-21 10:04:23 +0200

    Add initial work on CloudActivity

--- app/src/main/java/com/github/enteraname74/transposer/activities/CloudActivity.kt
+++ app/src/main/java/com/github/enteraname74/transposer/activities/CloudActivity.kt
@@ -1,12 +1,19 @@
 package com.github.enteraname74.transposer.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
 import com.github.enteraname74.transposer.R
 
 class CloudActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cloud)
+
+        val backButton = findViewById<ImageView>(R.id.back_button)
+        backButton.setOnClickListener { finish() }
+
+        val recyclerView = findViewById<RecyclerView>(R.id.scales_recycler_view)
     }
 }
